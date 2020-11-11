0f2f4473177635e62d13dafa8f51574d97a6fae9
Kieran <belli.kieran@gmail.com>
2020-11-11 17:13:23 +0100

    Add stats car

---
+++ src/Car/Fast.java
@@ -0,0 +1,12 @@
+package Car;
+
+public class Fast {
+    private int engine;
+    private int beforeLeftWheel;
+    private int beforeRightWheel;
+    private int backLeftWheel;
+    private int backRightWheel;
+
+
+
+}
---
+++ src/Car/Low.java
@@ -0,0 +1,9 @@
+package Car;
+
+public class Low {
+    private int engine;
+    private int beforeLeftWheel;
+    private int beforeRightWheel;
+    private int backLeftWheel;
+    private int backRightWheel;
+}
