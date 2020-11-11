d042dbe25e769e5941c58a89e6e8769020490e3d
Océane <oceanekegoff@gmail.com>
2020-11-11 16:54:09 +0100

    Creation random vitesse

--- src/Vitesse/DevinerVitesse.java
+++ src/Vitesse/DevinerVitesse.java
@@ -4,10 +4,22 @@ import java.util.Random;
 
 public class DevinerVitesse {
     String message[] = {"V", "I", "T", "E", "S", "S", "E"};
-    Random random= new Random();
+    String message2[] = {  message[Random(6)]= "V",
+                            message[Random(6)]= "I",
+                            message[Random(6)]= "T",
+                            message[Random(6)]= "E",
+                            message[Random(6)]= "S",
+                            message[Random(6)]= "S",
+                            message[Random(6)]= "E"};
+
+    public int Random(int max){
+    //nombre aléatoire
+        Random r = new Random();
+        return r.nextInt(max);
+    }
 
     public int [] InitTab(int tTaille) {
-        //création d'un tableau
+    //création d'un tableau
         int monTableau[] = new int[tTaille];
         for (int i = 0; i < tTaille; i++){
             monTableau[i] = i;
@@ -16,15 +28,27 @@ public class DevinerVitesse {
     }
 
     public void AfficherTab(int [] tab) {
-        //affiche tableau
+    //affichage d'un tableau tableau
         for (int i = 0; i < tab.length; i++) {
             System.out.print("| " +message[i]+ " |");
         }
     }
-    public static void tabVitesse(String args[]) {
-        DevinerVitesse devinerVitesse=new DevinerVitesse();
-        devinerVitesse.AfficherTab(devinerVitesse.InitTab(7));
 
+    public void AfficherTab2(int [] tab) {
+    //affichage du tableau modifié
+        for (int i = 0; i < tab.length; i++) {
+            System.out.print("| " +message2[i]+ " |");
+        }
+    }
+
+
+
+    public static void tabVitesse(String args[]) {
+    //affichage / appel des tableaux
+        DevinerVitesse devinerVitesse=new DevinerVitesse();
+        //devinerVitesse.AfficherTab(devinerVitesse.InitTab(7));
+
+        devinerVitesse.AfficherTab2(devinerVitesse.InitTab(7));
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,17 +2,30 @@ package com.company;
 
 import Menu.Menu;
 import Vitesse.DevinerVitesse;
-
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main  extends DevinerVitesse {
     Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args){
+        DevinerVitesse devinerVitesse= new DevinerVitesse();
         String message[] = {"V", "I", "T", "E", "S", "S", "E"};
+
+        int r = devinerVitesse.Random(6);
+        String message2[] = {
+                message[r]= "V",
+                message[r]= "I",
+                message[r]= "T",
+                message[r]= "E",
+                message[r]= "S",
+                message[r]= "S",
+                message[r]= "E"};
+
 
         //Menu.menu();
         DevinerVitesse.tabVitesse(message);
+        DevinerVitesse.tabVitesse(message2);
         Colors.choiceColors();
 
     }
