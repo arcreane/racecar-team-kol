52cbd3e39c7c44d97d8923de1e1f2c06e5e1020c
Loïc <loic.gouriou@edu.itescia.fr>
2020-11-12 20:46:42 +0100

    LE RENAISSANCE !!!!!!!!!

--- src/Car/CarPart/Motor.java
+++ src/Car/CarPart/Motor.java
@@ -5,32 +5,16 @@ import java.util.Random;
 public class Motor {
 
 
-
-    public class Engine{
-
-
-        String scrumbleEngine;
-
-        public int Random(int max) {
-            //nombre aléatoire
-            java.util.Random r = new Random();
-            return r.nextInt(max);
+    public String initTab() {
+        int maxTab = 5;
+        String nbList = "";
+        Random r = new Random();
+        for (int i = 0; i < maxTab; i++) {
+            nbList += r.nextInt(10);
         }
-
-        public String initTab() {
-            //création d'un tableau
-            int i;
-            scrumbleEngine = "";
-            Random r = new Random();
-            for (i = 0; i <= 5; i++) {
-                int index = r.nextInt(9);
-                scrumbleEngine += r;
-            }
-            return scrumbleEngine;
-        }
+        return nbList;
     }
 }
-
 
 
 
--- src/Car/FastCar.java
+++ src/Car/FastCar.java
@@ -1,7 +1,28 @@
 package Car;
 
+import Car.CarPart.Motor;
+
+import java.util.Scanner;
+
 public class FastCar extends Cars {
+    Motor motor;
     double random = Math.random();
+    @Override
+    public void check(){
+        motor = new Motor();
+        //String nbList = motor.initTab();
+        String nbList = "bonjour";
+        System.out.println(nbList);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Recopie la suite");
+        String comparaison = scanner.nextLine();
+
+        while (!(comparaison.equals(nbList))){
+                System.out.println("retry");
+                comparaison = scanner.nextLine();}
+
+        System.out.println("bravo");
 
 
+    }
 }
